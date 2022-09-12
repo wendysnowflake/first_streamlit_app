09b515d051e685576036fe5eda5a255960a0e05b
Wendy Reece <113147874+wendysnowflake@users.noreply.github.com>
2022-09-13 09:47:26 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block (called a function)
+  def get_fruityvice_data(this_fruit_choice):
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return (fruityvice_normalized)
+
 # New Section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,9 +39,8 @@ try:
    if not fruit_choice:
      streamlit.error("Please select a fruit to get information.")
    else:
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     streamlit.dataframe(fruityvice_normalized)
+     back_from_function = get_fruityvice_data(fruit_choice)
+     streamlit.dataframe(back_from_function)
 
 except URLError as e:
        streamlit.error()
@@ -44,11 +49,16 @@ except URLError as e:
 streamlit.stop()
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("Select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+#Snowflake-related finctions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("Select * from fruit_load_list")
+         return my_cur.fetchall()
+#Add a button to load the fruit
+ is streamlit.button(Get Fruit Load List'):
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
