480c9e03fafe8600278d536c1ba6c2d337c2670a
Wendy Reece <113147874+wendysnowflake@users.noreply.github.com>
2022-09-13 09:49:53 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
   def get_fruityvice_data(this_fruit_choice):
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     return (fruityvice_normalized)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return (fruityvice_normalized)
 
 # New Section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
